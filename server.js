6540ba4a0a75d59dae9b89882ee49d8c3146eeba
Blaise <ifollowjre@gmail.com>
2020-04-18 11:42:35 -0600

    added other version of server to new version

--- server.js
+++ server.js
@@ -4,7 +4,44 @@ const { join } = require("path");
 const morgan = require("morgan");
 const app = express();
 
+const mongoose = require("mongoose");
+const delegateRoutesFor = require("./routes");
+const bodyParser = require("body-parser");
 const port = process.env.SERVER_PORT || 3000;
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
+// Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static("client/build"));
+}
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/quizzly");
+// Add routes, both API and view
+
+delegateRoutesFor(app)
+
 
 app.use(morgan("dev"));
 app.use(express.static(join(__dirname, "build")));
