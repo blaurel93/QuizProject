04110ffaf725e86bd9e5e0405308c83c26071b6d
Blaise <ifollowjre@gmail.com>
2020-04-18 11:03:29 -0600

    creation of auth0 temp

---
+++ src/components/Content.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import { Row, Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import contentData from "../utils/contentData";
+///// THIS BELOW IS WHERE WE COULD PLACE ALL THE QUIZS /////
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="next-steps my-5">
+        <h2 className="my-5 text-center">Heres some quizzes</h2>
+        <Row className="d-flex justify-content-between">
+          
+          <card>
+            <h1>Hello</h1>
+            <p>this is a card</p>
+          </card>
+          
+          <card>
+            <h1>Hello</h1>
+            <p>this is a card</p>
+          </card>
+          <card>
+            <h1>Hello</h1>
+            <p>this is a card</p>
+          </card>
+        </Row>
+        <Row className="d-flex justify-content-between">
+          <card>
+            <h1>Hello</h1>
+            <p>this is a card</p>
+          </card>
+          <card>
+            <h1>Hello</h1>
+            <p>this is a card</p>
+          </card>
+          <card>
+            <h1>Hello</h1>
+            <p>this is a card</p>
+          </card>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import logo from "../assets/logo.svg";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    {/* <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" /> */}
+    <h1 className="mb-4">Quizzly</h1>
+
+    <p className="lead">
+      HERE IS WHERE YOU TAKE QUIZZES tO FIND OUT HOW DUMB YOU ARE
+    </p>
+  </div>
+);
+
+export default Hero;
