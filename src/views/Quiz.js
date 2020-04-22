9539655477d12773fa2c707b1564364bc8902058
Blaise <ifollowjre@gmail.com>
2020-04-22 16:44:51 -0600

    made cards for quizes and set state to console log so i can then render one specific form for the quiz

--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import axios from "axios";
+import "../assets/style.css"
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Container
+} from 'reactstrap';
 
 function Quiz() {
     const { loading, user } = useAuth0();
     const [state, setState] = useState({
         Pa1: "",
-        Pa2: ""
+        Pa2: "",
+        ActiveQuiz: "ShowAll",
+        DisplayQuiz: ""
     })
 
     const handleTyping = (e) => {
@@ -24,26 +31,87 @@ function Quiz() {
                 console.log("were back from the backend")
             })
     }
+ 
+    const handleSelectQuiz = (button) => {
+        switch (button) {
+            case 0:
+                setState({ ActiveQuiz: "Quiz0" });
+                console.log(state.ActiveQuiz)
+                break;
+            case 1:
+                setState({ ActiveQuiz: "Quiz1" });
+                console.log(state.ActiveQuiz)
+                break;
+            case 2:
+                setState({ ActiveQuiz: "Quiz2" });
+                console.log(state.ActiveQuiz)
+                break;
+            case 3:
+                setState({ ActiveQuiz: "Quiz3" });
+                console.log(state.ActiveQuiz)
+                break;
+            case 4:
+                setState({ ActiveQuiz: "Quiz4" });
+                console.log(state.ActiveQuiz)
+                break;
+            default:
+                setState({ ActiveQuiz: "ShowAll" });
+        }
+    }
 
     console.log("this is our state", state)
     return (
-        <div>
-            <div>
-                <h1> Hello from my quiz!!</h1>
-                <p name="Planet">The Universe</p>
+        <Container>
+            {/* {state.ActiveQuiz()}  CHECK THE STATE HERE       isthistrue  ?() : ()*/}
+            <div className="cardS">
+
+                <Card className="boox">
+                    <CardImg top width="100%" src="../assets/output-onlinepngtools.png" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>This is 0</CardTitle>
+                        <CardSubtitle>Quiz 0</CardSubtitle>
+                        <CardText>This is defiantly quiz 0</CardText>
+                        <Button onClick={() => { handleSelectQuiz(0) }}>Button</Button>
+                    </CardBody>
+                </Card>
+                <Card className="boox">
+                    <CardImg top width="100%" src="../assets/output-onlinepngtools.png" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>This is 1</CardTitle>
+                        <CardSubtitle>Quiz 1</CardSubtitle>
+                        <CardText>This is quiz 1</CardText>
+                        <Button onClick={() => { handleSelectQuiz(1) }}>Button</Button>
+                    </CardBody>
+                </Card>
+                <Card className="boox">
+                    <CardImg top width="100%" src="../assets/output-onlinepngtools.png" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>This is 2</CardTitle>
+                        <CardSubtitle>QUiz 2</CardSubtitle>
+                        <CardText>This quiz 2</CardText>
+                        <Button onClick={() => { handleSelectQuiz(2) }}>Button</Button>
+                    </CardBody>
+                </Card>
+                <Card className="boox">
+                    <CardImg top width="100%" src="../assets/output-onlinepngtools.png" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>This is 3</CardTitle>
+                        <CardSubtitle>Quiz 3</CardSubtitle>
+                        <CardText>This quizx 3</CardText>
+                        <Button onClick={() => { handleSelectQuiz(3) }}>Button</Button>
+                    </CardBody>
+                </Card>
+                <Card className="boox">
+                    <CardImg top width="100%" src="../assets/output-onlinepngtools.png" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>This is 4</CardTitle>
+                        <CardSubtitle>Quiz 4</CardSubtitle>
+                        <CardText>This quiz 4</CardText>
+                        <Button onClick={() => { handleSelectQuiz(4) }}>Button</Button>
+                    </CardBody>
+                </Card>
             </div>
-            <div>
-                <p name="p1">How far is the Earth from the Sun?</p>
-                <input name="Pa1" onChange={handleTyping}></input>
-            </div>
-            <div>
-                <p name="p2">How far is the Earth from the Sun?</p>
-                <input name="Pa2" onChange={handleTyping}></input>
-            </div>
-            <div>
-                <button name="button" onClick={handleSubmit}>Submit</button>
-            </div>
-        </div>
+        </Container>
     )
 }
 
