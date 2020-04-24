0189f8d9e4131a59be45377272b38293a5b8afa3
Blaise <ifollowjre@gmail.com>
2020-04-23 19:59:17 -0600

    building blocks for quizs

--- src/components/Quiz3.js
+++ src/components/Quiz3.js
@@ -1,12 +1,147 @@
 import React from "react";
 import "../assets/style.css"
-import {
-    Card
-} from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Quiz3 = () => (
-  <Card>
-      <h1>Hello 3</h1>
-  </Card>
+    <Form>
+    <h1>Quiz 4</h1>
+    <FormGroup tag="fieldset">
+        <legend>Question 1</legend>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option one 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option two 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option three
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option four
+          </Label>
+        </FormGroup>
+    </FormGroup>
+    <FormGroup tag="fieldset">
+        <legend>Question 2</legend>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option one 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option two 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option three
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option four
+          </Label>
+        </FormGroup>
+    </FormGroup>
+    <FormGroup tag="fieldset">
+        <legend>Question 3</legend>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option one 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option two 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option three
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option four
+          </Label>
+        </FormGroup>
+    </FormGroup>
+    <FormGroup tag="fieldset">
+        <legend>Question 4</legend>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option one 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option two 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option three
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option four
+          </Label>
+        </FormGroup>
+    </FormGroup>
+    <FormGroup tag="fieldset">
+        <legend>Question 5</legend>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option one 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option two 
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" name="radio1" />{' '}
+            Option three
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option four
+          </Label>
+        </FormGroup>
+    </FormGroup>
+    <Button>Submit</Button>
+    
+</Form>
 );
 export default Quiz3;
