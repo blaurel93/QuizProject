f49524f45c88372558ef48ec6d6b1a9fa495fbb3
Atravias Borley <atravias@gmail.com>
2020-04-23 21:25:11 -0600

    got all quizzes filled in

--- src/components/Quiz0.js
+++ src/components/Quiz0.js
@@ -7,64 +7,64 @@ import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap
 const Quiz0 = () => (
 
     <Form>
-        <h1 className="Quiz">Quiz 1</h1>
+        <h1 className="Quiz">How well do you know Jalapenos?</h1>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>Which of the following is true?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>Answer 1</option>
-                    <option>Bullshit 2 </option>
-                    <option>Fuck me 3</option>
-                    <option>Why 4</option>
+                    <option className="correctAnswer">Jalapenos are one of the best sources of vitamin C</option>
+                    <option>Jalapenos are used as bullets</option>
+                    <option>Jalapenos are not known to be spicy</option>
+                    <option>The seeds in a jalapeno are the HOTTEST part of the pepper</option>
                     <option>Couldn't it be 5</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>Which of the following is true?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>You cant grow jalapenos</option>
+                    <option>Jalapenos can only grow in the desert</option>
+                    <option>Jalapenos grow up to 6 feet long! But they shrink after picking</option>
+                    <option className="correctAnswer">After harvest if Jalapenos are stored at 45°F they have a shelf life of up to 3-5 weeks</option>
+                    <option>Jalapenos are dark blue</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>Which of the following is true?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>jalapenos will blind you</option>
+                    <option>Jalapenos can hit the woah</option>
+                    <option className="correctAnswer">Jalapenos help lower blood pressure, fight migraine headaches and lose weight</option>
+                    <option>Jalapeno Gigante II can grow up to 15 inches!!</option>
+                    <option>They taste like chicken!</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>Which of the following is true?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Bell peppers are the same as jalapenos</option>
+                    <option className="correctAnswer">About 40,000 acres are dedicated for the cultivation of Jalapenos in Mexico</option>
+                    <option>jalapenos are actually eggs from a green animal</option>
+                    <option>Leprechauns try to steal them</option>
+                    <option>They taste great with peanut butter!</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>Which of the following is true?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Jalapenos are used as a dessert for kids!</option>
+                    <option>Jalapenos rot your teeth</option>
+                    <option>Jalapenos make your feet stink!</option>
+                    <option>Jalapenos make you want to eat grass</option>
+                    <option className="correctAnswer">August 19th is National Hot and Spicy Food Day</option>
                 </Input>
             </Col>
         </FormGroup>
--- src/components/Quiz2.js
+++ src/components/Quiz2.js
@@ -6,62 +6,62 @@ const Quiz2 = () => (
     <Form>
         <h1 className="Quiz"> Quiz 3</h1>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>Where were bicycles first introduced?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>Answer 1</option>
-                    <option>Bullshit 2 </option>
-                    <option>Fuck me 3</option>
-                    <option>Why 4</option>
-                    <option>Couldn't it be 5</option>
+                    <option className="correctAnswer">Europe</option>
+                    <option>Germany</option>
+                    <option>US</option>
+                    <option>Brazil</option>
+                    <option>China</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>What's the core of a bicycle called?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Bracket</option>
+                    <option>Pedal</option>
+                    <option className="correctAnswer">Frame</option>
+                    <option>Wheel</option>
+                    <option>Horn</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>What does the front fork hold?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>The back wheel</option>
+                    <option className="correctAnswer">The front wheel</option>
+                    <option>The chain</option>
+                    <option>The pedals</option>
+                    <option>The back</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>What do bicycles use to reduce friction?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Rubber</option>
+                    <option>Pedals</option>
+                    <option>Brakes</option>
+                    <option>Air resistance</option>
+                    <option className="correctAnswer">Ball bearings</option>
                 </Input>
             </Col>
         </FormGroup>
         <FormGroup row>
-            <Label for="exampleSelect" sm={2}>What do you want to ask?</Label>
+            <Label for="exampleSelect" sm={2}>Which bicycle part doesn't have ball bearings?</Label>
             <Col sm={10}>
                 <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Rear hub</option>
+                    <option>Front hub</option>
+                    <option>Pedals</option>
+                    <option className="correctAnswer">Gear shift</option>
+                    <option>Rear wheel</option>
                 </Input>
             </Col>
         </FormGroup>
